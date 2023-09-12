6d00585da2ea064f78a6ba638505e2d17515c55f
Vizaxo <26389755+Vizaxo@users.noreply.github.com>
2023-09-12 22:24:11 +0100

    Start adding keyboard input support

---
+++ src/input/Keyboard.cpp
@@ -0,0 +1,9 @@
+#include "Keyboard.h"
+
+Keyboard::Key Keyboard::getKey(WPARAM wParam) {
+	if (wParam >= 0x30 && wParam <= 0x39) {
+		return static_cast<Key>(wParam - 0x30 + static_cast<WPARAM>(Key::NUM0));
+	} else if (wParam >= 0x41 && wParam <= 0x5A) {
+		return static_cast<Key>(wParam - 0x41 + static_cast<WPARAM>(Key::A));
+	}
+}
---
+++ src/input/Keyboard.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "DirectXTemplatePCH.h"
+
+namespace Keyboard {
+enum class Key {
+	NUM0,
+	NUM1,
+	NUM2,
+	NUM3,
+	NUM4,
+	NUM5,
+	NUM6,
+	NUM7,
+	NUM8,
+	NUM9,
+	A,
+	B,
+	C,
+	D,
+	E,
+	F,
+	G,
+	H,
+	I,
+	J,
+	K,
+	L,
+	M,
+	N,
+	O,
+	P,
+	Q,
+	R,
+	S,
+	T,
+	U,
+	V,
+	W,
+	X,
+	Y,
+	Z,
+	Unknown,
+};
+
+Key getKey(WPARAM wParam);
+
+}
