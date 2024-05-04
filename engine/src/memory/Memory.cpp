d9d00dff24517e98704a27a34013702805de98d7
Vizaxo <26389755+Vizaxo@users.noreply.github.com>
2024-05-04 22:14:50 +0100

    Clean up allocation printing

--- engine/src/memory/Memory.cpp
+++ engine/src/memory/Memory.cpp
@@ -5,16 +5,26 @@
 
 #if _DEBUG
 static bool debugPrintAllocationStacks = false;
+static bool debugPrintAllocations = true;
 #endif
 
 void* operator new(size_t n) {
+	void* addr = malloc(n);
+
 #if _DEBUG
+	char buf[50];
+
 	if (debugPrintAllocationStacks) {
-		char buf[50];
 		snprintf(buf, 50, "Allocated %lld bytes\n", n);
 		PAL::printBacktrace(12, 2);
 		DEBUG_PRINT(buf);
 	}
+
+	if (debugPrintAllocations) {
+		snprintf(buf, 50, "Allocated %lld bytes at %p\n", n, addr);
+		DEBUG_PRINT(buf);
+	}
 #endif
-	return malloc(n);
+
+	return addr;
 }
