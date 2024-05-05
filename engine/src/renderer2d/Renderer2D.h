5f125ce76b5c3a2b5e56710c2d61ee168ad0d84c
Vizaxo <26389755+Vizaxo@users.noreply.github.com>
2024-05-05 23:01:13 +0100

    Randomly spawn circles in 2d physics simulation

--- engine/src/renderer2d/Renderer2D.h
+++ engine/src/renderer2d/Renderer2D.h
@@ -19,6 +19,10 @@ struct Circle {
 	float mass;
 };
 
+static inline float randf() {
+	return 2.0*((float)rand() / (float)(RAND_MAX)) - 1.0;
+}
+
 struct Scene2D {
 	std::vector<Circle> circles;
 	Box2D viewport;
@@ -26,6 +30,10 @@ struct Scene2D {
 	Scene2D() {
 		circles.emplace_back(Circle({{100, 100}, 20, {1, 1, 0}}));
 		circles.emplace_back(Circle({{110, 200}, 20, {1, 1, 0}, {0.0, 5.0}}));
+
+		for (int i = 0; i < 20; ++i) {
+			circles.emplace_back(Circle({ {randf()*200 + 400, randf()*200 + 400}, 20, {1, 1, 0}, v2f({(float)(randf()*5.0), (float)(randf()*5.0)}), 1.0 }));
+		}
 		viewport = {{-100, -100}, {100, 100}};
 	}
 };
