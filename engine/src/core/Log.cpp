3df2f0af579e397671abf57e55dea59b1b49a430
Vizaxo <26389755+Vizaxo@users.noreply.github.com>
2024-05-11 01:59:54 +0100

    Print channel name with log

--- engine/src/core/Log.cpp
+++ engine/src/core/Log.cpp
@@ -20,12 +20,12 @@ void Log::logStr(Level lvl, Channel& chan, int line, const char* file, const cha
 	va_list args;
 	va_start(args, fmt);
 	switch (lvl) {
-	case INFO: DEBUG_PRINT("INFO: "); break;
-	case WARN: DEBUG_PRINT("WARN: "); break;
-	case FATAL: DEBUG_PRINT("FATAL: "); break;
+	case INFO: DEBUG_PRINT("INFO "); break;
+	case WARN: DEBUG_PRINT("WARN "); break;
+	case FATAL: DEBUG_PRINT("FATAL "); break;
 	default: return; //TODO: error
 	}
-	snprintf(buf, BUFSIZE, "%s (%d): ", file, line);
+	snprintf(buf, BUFSIZE, "%s: %s (%d): ", chan, file, line);
 	DEBUG_PRINT(buf);
 
 	vsnprintf(buf, BUFSIZE, fmt, args);
