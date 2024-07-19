1a8bf84b2c4688d971e6cbf8f105d0f75b9289f3
Vizaxo <26389755+Vizaxo@users.noreply.github.com>
2024-07-19 20:35:23 +0100

    Add Vector4

--- engine/src/types/Vector.h
+++ engine/src/types/Vector.h
@@ -53,13 +53,6 @@ struct Vector3 {
 	T z;
 };
 
-template <>
-struct Vector3<float> {
-	float x;
-	float y;
-	float z;
-};
-
 template <typename T> inline T lenSquared(Vector3<T> a) {
 	return a.x*a.x + a.y*a.y + a.z*a.z;
 }
@@ -85,3 +78,41 @@ typedef Vector3<double> v3d;
 typedef Vector3<int> v3i;
 
 typedef v3f v3;
+
+
+
+
+
+template <typename T>
+struct Vector4 {
+	T x;
+	T y;
+	T z;
+	T w;
+};
+
+template <typename T> inline T lenSquared(Vector4<T> a) {
+	return a.x*a.x + a.y*a.y + a.z*a.z + a.w*a.w;
+}
+
+template <typename T> inline Vector4<T> normalize(Vector4<T> a) {
+	float lenSq = lenSquared(a);
+	float len = std::sqrt(lenSq);
+	return {a.x / len, a.y / len, a.z / len, a.w / len};
+}
+
+template <typename T> inline Vector4<T> floor(Vector4<T> in) { return {std::floor(in.x), std::floor(in.y), std::floor(in.z), std::floor(in.w)}; }
+template <typename T> inline Vector4<T> ceil(Vector4<T> in) { return {std::ceil(in.x), std::ceil(in.y), std::ceil(in.z), std::ceil(in.w)}; }
+template <typename T> inline T dot(Vector4<T> a, Vector4<T> b) { return a.x*b.x+a.y*b.y+a.z*b.z+a.w*b.w; }
+
+template <typename T> inline Vector4<T> operator+(Vector4<T> a, Vector4<T> b) { return {a.x+b.x, a.y+b.y, a.z+b.z, a.w+b.w}; }
+template <typename T> inline Vector4<T> operator-(Vector4<T> a, Vector4<T> b) { return {a.x-b.x, a.y-b.y, a.z-b.z, a.w-b.w}; }
+
+template <typename T> inline Vector4<T> operator*(Vector4<T> a, T b) { return {a.x*b, a.y*b, a.z*b, a.w*b}; }
+template <typename T> inline Vector4<T> operator/(Vector4<T> a, T b) { return {a.x/b, a.y/b, a.z/b, a.w/b}; }
+
+typedef Vector4<float> v4f;
+typedef Vector4<double> v4d;
+typedef Vector4<int> v4i;
+
+typedef v4f v4;
