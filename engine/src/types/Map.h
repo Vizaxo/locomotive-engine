265733418a530aa48113a76bb82396e1a1f877fa
Vizaxo <26389755+Vizaxo@users.noreply.github.com>
2024-07-21 21:21:01 +0100

    Map improvements ready for ECS

--- engine/src/types/Map.h
+++ engine/src/types/Map.h
@@ -7,14 +7,17 @@
 
 template <typename K, typename V, typename H=DefaultHash<K>>
 struct HashMap {
-	using Entry = Product<K, V>;
+	struct Entry {
+		K key;
+		V value;
+	};
 
 	struct EntryInternal {
 		Entry e;
 		bool active;
 	};
 
-	Entry* data;
+	EntryInternal* data = nullptr;
 	u32 n = 0;
 	u32 m = 0;
 
@@ -22,21 +25,25 @@ struct HashMap {
 	static constexpr float loadFactorMax = 0.7f;
 
 	RefPtr<Entry> insert(K& k, V& v);
+	RefPtr<Entry> insert(K k, V v);
+	RefPtr<Entry, true> get(K&& k);
 	RefPtr<Entry, true> get(K& k);
 	void resizeIfNeeded();
 
-	RefPtr<Product<K, V>> insertNoResize(Entry& e);
+	RefPtr<Entry> insertNoResize(EntryInternal&& e);
 };
 
 template <typename K, typename V, typename H>
 void HashMap<K, V, H>::resizeIfNeeded() {
 	float loadFactor = ((float)n+1)/(float)m;
-	if (loadFactor > loadFactorMax) {
-		u32 newM = m*2;
-		Entry* newData = calloc(sizeof(Entry) * newM);
+
+	if (!data || loadFactor > loadFactorMax) {
+		u32 newM = m == 0 ? 4 : m*2;
+		EntryInternal* newData = (EntryInternal*)calloc(newM, sizeof(EntryInternal));
+		ASSERT(newData, "Calloc failed");
 
 		for (int i = 0; i < m; ++i) {
-			Entry& e = data[i];
+			EntryInternal& e = data[i];
 			if (e.active) {
 				insertNoResize(std::move(e));
 			}
@@ -49,33 +56,50 @@ void HashMap<K, V, H>::resizeIfNeeded() {
 }
 
 template <typename K, typename V, typename H>
-RefPtr<Product<K, V>, true> HashMap<K, V, H>::get(K& k) {
+RefPtr<typename HashMap<K, V, H>::Entry, true> HashMap<K, V, H>::get(K&& k) {
+	return get(k);
+}
+
+template <typename K, typename V, typename H>
+RefPtr<typename HashMap<K, V, H>::Entry, true> HashMap<K, V, H>::get(K& k) {
+	if (m == 0) return nullptr;
 	u32 hash = H{}(k);
 
-	u32 index = hash;
-	while (data[index].k != k) {
+	u32 index = hash % m;
+	while (data[index].active && data[index].e.key != k) {
 		++index;
 		index %= m;
 	}
 
-	return &data[index];
+	if (!data[index].active)
+		return nullptr;
+
+	return &data[index].e;
 }
 
 template <typename K, typename V, typename H>
-RefPtr<Product<K, V>> HashMap<K, V, H>::insertNoResize(Entry& e) {
-	u32 hash = H{}(e.key);
+RefPtr<typename HashMap<K, V, H>::Entry> HashMap<K, V, H>::insertNoResize(EntryInternal&& e) {
+	u32 hash = H{}(e.e.key);
 
-	u32 insertIndex = hash;
+	u32 insertIndex = hash % m;
+	ASSERT(m != 0, "");
 	while (data[insertIndex].active) {
 		++insertIndex;
 		insertIndex %= m;
 	}
 	data[insertIndex] = std::move(e);
+	n++;
 	return &data[insertIndex].e;
 }
 
 template <typename K, typename V, typename H>
-RefPtr<Product<K, V>> HashMap<K, V, H>::insert(K& k, V& v) {
+RefPtr<typename HashMap<K, V, H>::Entry> HashMap<K, V, H>::insert(K& k, V& v) {
 	resizeIfNeeded();
 	return insertNoResize(Entry{std::move(k), std::move(v), true});
+}
+
+template <typename K, typename V, typename H>
+RefPtr<typename HashMap<K, V, H>::Entry> HashMap<K, V, H>::insert(K k, V v) {
+	resizeIfNeeded();
+	return insertNoResize({{std::move(k), std::move(v)}, true});
 }
