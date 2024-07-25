2529dabc454cfd895aec046286bd8a4841e38e7b
Vizaxo <26389755+Vizaxo@users.noreply.github.com>
2024-07-26 00:01:00 +0100

    Add missing keycodes for camera movement

--- engine/src/platform/windows/WinInput.cpp
+++ engine/src/platform/windows/WinInput.cpp
@@ -11,15 +11,24 @@ namespace PAL {
 namespace Windows {
 
 Keyboard::Key getKey(WPARAM wParam) {
-	if (wParam >= 0x30 && wParam <= 0x39) {
+	if (wParam >= 0x30 && wParam <= 0x39)
 		return static_cast<Keyboard::Key>(wParam - 0x30 + static_cast<WPARAM>(Keyboard::Key::NUM0));
-	} else if (wParam >= 0x41 && wParam <= 0x5A) {
+	if (wParam >= 0x41 && wParam <= 0x5A)
 		return static_cast<Keyboard::Key>(wParam - 0x41 + static_cast<WPARAM>(Keyboard::Key::A));
-	} else if (wParam >= VK_F1 && wParam <= VK_F24) {
+	if (wParam >= VK_F1 && wParam <= VK_F24)
 		return static_cast<Keyboard::Key>(wParam - VK_F1 + static_cast<WPARAM>(Keyboard::Key::F1));
+
+	switch (wParam) {
+	case VK_OEM_COMMA:		return Keyboard::Comma;
+	case VK_OEM_PERIOD:		return Keyboard::Period;
+	case VK_OEM_7:			return Keyboard::SingleQuote;
+	case VK_LEFT:			return Keyboard::ArrowLeft;
+	case VK_RIGHT:			return Keyboard::ArrowRight;
+	case VK_UP:				return Keyboard::ArrowUp;
+	case VK_DOWN:			return Keyboard::ArrowDown;
+	default:				return Keyboard::Key::Unknown;
 	}
 
-	return Keyboard::Key::Unknown;
 }
 
 /*
