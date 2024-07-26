89772695afc72545ea85ea3f3583a9f2357b19a0
Vizaxo <26389755+Vizaxo@users.noreply.github.com>
2024-07-26 22:17:32 +0100

    Move heightmap shader out of engine

--- engine/src/shaders/DrawHeightmap.hlsl
+++
@@ -1,8 +0,0 @@
-Texture2D<float> heightmap : register(t0);
-SamplerState heightmapSampler : register(s0);
-
-float4 main(in float4 pos : SV_POSITION, in float2 uv : TEXCOORD) : SV_TARGET
-{
-	float h = heightmap.Sample(heightmapSampler, uv) / 100.f + 0.5f;
-	return float4(0.f, h, 0.f, h);
-}
